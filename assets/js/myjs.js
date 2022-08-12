faa1be1db88610d5bffceae09c12e44812c5b91e
ShubhanjanMedhi-dev <shubhanjan@squadcast.com>
2022-08-12 22:04:00 +0530

    all admin functionalities added

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -45,6 +45,21 @@ if(window.location.pathname.includes('edit-property')){
     }
 }
 
+if(window.location.pathname.includes('add-agent')){
+    var submitButton = document.getElementById('submitButton');
+    submitButton.onclick = function (){
+        addAgency();
+    }
+}
+
+if(window.location.pathname.includes('edit-agent')){
+    var id = window.location.search.split('?')[1];
+    var submitButton = document.getElementById('submitButton');
+    submitButton.onclick = function(){
+        editAgency(id);
+    }
+}
+
 //Add property page wizard validation starts (backend)
 
 
@@ -267,7 +282,7 @@ function deleteProperty(id){
 
 //Delete property wizard ends (backend)
 
-//Get property wizard starts (backend)
+//Get single property wizard starts (backend)
 
 function getProperty(id){
     const params = {
@@ -292,7 +307,7 @@ function getProperty(id){
     }
 }
 
-//Get property wizard ends (backend)
+//Get single property wizard ends (backend)
 
 //Get all property wizard starts (backend)
 
@@ -301,7 +316,6 @@ function getAllProperties(currentPage,recordsPerPage){
     const http = new XMLHttpRequest();
     try{
         http.open('GET',url+'/v1/properties?currentpage='+currentPage+'&recordsPerPage='+recordsPerPage);
-        //http.setRequestHeader('ngrok-skip-browser-warning',true);
         http.setRequestHeader('Content-type', 'application/json');
         http.send();
         http.onload = function(){
@@ -311,7 +325,6 @@ function getAllProperties(currentPage,recordsPerPage){
             else{
                 var res = JSON.parse(http.response);
                 allPropertiesCount = res.count;
-                allProperties = res;
 
                 if(currentPage == 1){
                     currentCount = allPropertiesCount;
@@ -340,18 +353,18 @@ function getAllProperties(currentPage,recordsPerPage){
 //Show all properties starts (backend)
 
 function showAllProperties(recordsPerPage){
-        var htmlElement = document.getElementById('htmlElement');
-        var htmlElementVar = '';
-        var imagesElement = '';
-        var htmlPagination = document.getElementById('htmlPagination');
-        var noOfPages = allPropertiesCount / recordsPerPage;
-        var paginationElements = '';
+    var htmlElement = document.getElementById('htmlElement');
+    var htmlElementVar = '';
+    var imagesElement = '';
+    var htmlPagination = document.getElementById('htmlPagination');
+    var noOfPages = allPropertiesCount / recordsPerPage;
+    var paginationElements = '';
 
-        if(noOfPages > Math.round(noOfPages)){
-            noOfPages = Math.round(noOfPages)+1;
-        }else{
-            noOfPages = Math.round(noOfPages);
-        }
+    if(noOfPages > Math.round(noOfPages)){
+        noOfPages = Math.round(noOfPages)+1;
+    }else{
+        noOfPages = Math.round(noOfPages);
+    }
 
     for(j=0; j<allProperties.length; j++){
 
@@ -360,14 +373,14 @@ function showAllProperties(recordsPerPage){
         }
 
         htmlElementVar = htmlElementVar+'<div class="col-xl-4 col-md-6 xl-6"><div class="property-box"><div class="property-image"><div class="property-slider">'+
-        imagesElement+'</div><div class="labels-left"><div><span class="label label-shadow">'+
-        allProperties[j].propertyStatus+'</span></div></div><div class="overlay-property-box"><a href=javascript:deleteProperty("'+allProperties[j]._id+'") class="effect-round" data-bs-toggle="tooltip" data-bs-placement="left" title="Delete">'+
-        '<i data-feather="trash-2"></i></a><a href="edit-property.html?'+allProperties[j]._id+'" class="effect-round like" data-bs-toggle="tooltip" data-bs-placement="left" title="Edit"><i data-feather="edit"></i></a></div></div>'+
-        '<div class="property-details"><span class="font-roboto">'+allProperties[j].city+'</span><a href="../main/single-property.html?'+allProperties[j]._id+'"><h3>'+allProperties[j].propertyType+' '+allProperties[j].propertyStatus+
-        '</h3></a><h6>$'+allProperties[j].price+'</h6><p class="font-roboto light-font">'+allProperties[j].description+'</p><ul><li><img src="../assets/images/svg/icon/double-bed.svg" class="img-fluid" alt="">Bed : '+
-        allProperties[j].beds+'</li><li><img src="../assets/images/svg/icon/bathroom.svg" class="img-fluid" alt="">Baths : '+allProperties[j].baths+'</li><li><img src="../assets/images/svg/icon/square-ruler-tool.svg" class="img-fluid ruler-tool" alt="">Area : '+
-        allProperties[j].area+' Sq. Ft.</li></ul><div class="property-btn d-flex"><button type="button"  onclick=details("'+allProperties[j]._id+'") class="btn btn-dashed btn-pill color-2">Details</button>'+
-        '</div></div></div></div>';
+            imagesElement+'</div><div class="labels-left"><div><span class="label label-shadow">'+
+            allProperties[j].propertyStatus+'</span></div></div><div class="overlay-property-box"><a href=javascript:deleteProperty("'+allProperties[j]._id+'") class="effect-round" data-bs-toggle="tooltip" data-bs-placement="left" title="Delete">'+
+            '<i data-feather="trash-2"></i></a><a href="edit-property.html?'+allProperties[j]._id+'" class="effect-round like" data-bs-toggle="tooltip" data-bs-placement="left" title="Edit"><i data-feather="edit"></i></a></div></div>'+
+            '<div class="property-details"><span class="font-roboto">'+allProperties[j].city+'</span><a href="../main/single-property.html?'+allProperties[j]._id+'"><h3>'+allProperties[j].propertyType+' '+allProperties[j].propertyStatus+
+            '</h3></a><h6>$'+allProperties[j].price+'</h6><p class="font-roboto light-font">'+allProperties[j].description+'</p><ul><li><img src="../assets/images/svg/icon/double-bed.svg" class="img-fluid" alt="">Bed : '+
+            allProperties[j].beds+'</li><li><img src="../assets/images/svg/icon/bathroom.svg" class="img-fluid" alt="">Baths : '+allProperties[j].baths+'</li><li><img src="../assets/images/svg/icon/square-ruler-tool.svg" class="img-fluid ruler-tool" alt="">Area : '+
+            allProperties[j].area+' Sq. Ft.</li></ul><div class="property-btn d-flex"><button type="button"  onclick=details("'+allProperties[j]._id+'") class="btn btn-dashed btn-pill color-2">Details</button>'+
+            '</div></div></div></div>';
 
     }
 
@@ -383,6 +396,10 @@ function showAllProperties(recordsPerPage){
 function details(id){
     window.location.href = '../main/single-property.html?'+id;
 }
+
+//Show all properties end (backend)
+
+//Function to load all scripts starts
 
 function loadScript(){
     var script9 = document.createElement('script');
@@ -405,8 +422,7 @@ function loadScript(){
     script6.src = '../assets/js/custom-slick.js';
     var script10 = document.createElement('script');
     script10.src = '../assets/js/admin-script.js';
-    var script7 = document.createElement('script');
-    script7.src = '../assets/js/customizer.js';
+    
     document.body.appendChild(script9);
     document.body.appendChild(script8);
     document.body.appendChild(script);
@@ -417,7 +433,316 @@ function loadScript(){
     document.body.appendChild(script5);
     document.body.appendChild(script6);
     document.body.appendChild(script10);
-    document.body.appendChild(script7);
 }
 
-//Show all properties end (backend)
+//Function to load all scripts ends
+
+//Show trash starts (backend)
+
+function showTrash(currentPage, recordsPerPage){
+    const http = new XMLHttpRequest();
+    try{
+        http.open('GET',url+'/v1/properties?currentpage='+currentPage+'&recordsPerPage='+recordsPerPage);
+        http.setRequestHeader('Content-type', 'application/json');
+        http.send();
+        http.onload = function(){
+            if(http.status != 200 && http.status != 201){
+                console.log('API status: '+http.status);
+            }
+            else{
+                var res = JSON.parse(http.response);
+                allPropertiesCount = res.count;
+
+                if(currentPage == 1){
+                    currentCount = allPropertiesCount;
+                }
+
+                allProperties = res.data;
+
+                if(currentCount != 0){
+                    showAllTrash(recordsPerPage);
+                }
+
+                if(currentCount > recordsPerPage){
+                    currentCount = currentCount - recordsPerPage;
+                }else{
+                    currentCount = currentCount - currentCount;
+                }
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+function showAllTrash(recordsPerPage){
+    var htmlElement = document.getElementById('htmlElement');
+    var htmlElementVar = '';
+    var imagesElement = '';
+    var htmlPagination = document.getElementById('htmlPagination');
+    var noOfPages = allPropertiesCount / recordsPerPage;
+    var paginationElements = '';
+
+    if(noOfPages > Math.round(noOfPages)){
+        noOfPages = Math.round(noOfPages)+1;
+    }else{
+        noOfPages = Math.round(noOfPages);
+    }
+
+    for(j=0; j<allProperties.length; j++){
+
+        for(i=0; i<allProperties[j].media.length; i++){
+            imagesElement = imagesElement+'<a href="javascript:void(0)"><img src="'+url+allProperties[j].media[i]+'" class="bg-img" alt=""></a>';
+        }
+
+        htmlElementVar = htmlElementVar+'<div class="col-xl-4 col-md-6 xl-6"><div class="property-box"><div class="property-image"><div class="property-slider">'+
+            imagesElement+'</div><div class="labels-left"><div><span class="label label-shadow">'+
+            allProperties[j].propertyStatus+'</span></div></div><div class="overlay-property-box"><a href=javascript:restoreProperty("'+allProperties[j]._id+'") class="effect-round" data-bs-toggle="tooltip" data-bs-placement="left" title="Delete">'+
+            '<i data-feather="upload-cloud"></i></a><a href="edit-property.html?'+allProperties[j]._id+'" class="effect-round like" data-bs-toggle="tooltip" data-bs-placement="left" title="Edit"><i data-feather="edit"></i></a></div></div>'+
+            '<div class="property-details"><span class="font-roboto">'+allProperties[j].city+'</span><a href="../main/single-property.html?'+allProperties[j]._id+'"><h3>'+allProperties[j].propertyType+' '+allProperties[j].propertyStatus+
+            '</h3></a><h6>$'+allProperties[j].price+'</h6><p class="font-roboto light-font">'+allProperties[j].description+'</p><ul><li><img src="../assets/images/svg/icon/double-bed.svg" class="img-fluid" alt="">Bed : '+
+            allProperties[j].beds+'</li><li><img src="../assets/images/svg/icon/bathroom.svg" class="img-fluid" alt="">Baths : '+allProperties[j].baths+'</li><li><img src="../assets/images/svg/icon/square-ruler-tool.svg" class="img-fluid ruler-tool" alt="">Area : '+
+            allProperties[j].area+' Sq. Ft.</li></ul><div class="property-btn d-flex"><button type="button"  onclick=details("'+allProperties[j]._id+'") class="btn btn-dashed btn-pill color-2">Details</button>'+
+            '</div></div></div></div>';
+
+    }
+
+    for(i=1; i<=noOfPages; i++){
+        paginationElements = paginationElements+'<li class="page-item"><a class="page-link" href="javascript:getAllProperties('+i+',6)">'+i+'</a></li>';
+    }
+
+    htmlElement.innerHTML = htmlElementVar;
+    htmlPagination.innerHTML = '<ul class="pagination">'+paginationElements+'</ul>';
+    loadScript();
+}
+
+//Show trash ends (backend)
+
+//Restore property wizard starts (backend)
+
+function restoreProperty(id){
+    const http = new XMLHttpRequest();
+    try{
+        http.open('POST',url+'/v1/properties/'+id);
+        http.setRequestHeader('Content-type', 'application/json');
+        http.send();
+        http.onload = function(){
+            if(http.status != 200 && http.status != 201){
+                console.log('API status: '+http.status);
+            }
+            else{
+                window.location.href = 'listing.html';
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+//Restore property wizard ends (backend)
+
+//Show tickets wizard starts (backend)
+
+function showTicket(){
+    const http = new XMLHttpRequest();
+    try{
+        http.open('GET',url+'/v1/tickets');
+        http.setRequestHeader('Content-type', 'application/json');
+        http.send();
+        http.onload = function(){
+            if(http.status != 200 && http.status != 201){
+                console.log('API status: '+http.status);
+            }
+            else{
+                var res = JSON.parse(http.response);
+                allPropertiesCount = res.count;
+                allProperties = res.data;
+                showAllTickets();
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+function showAllTickets(){
+    var htmlElement = document.getElementById('htmlElement');
+    var htmlElementVar = '';
+
+    for(i=0; i<allProperties.length; i++){
+        htmlElementVar = htmlElementVar+'<tr><td><div class="media"><div class="media-body"><h6>'+allProperties[i].name+'</h6></div></div></td><td class="light-font">'+
+            allProperties[i].phone+'</td><td class="light-font">'+allProperties[i].email+'</td><td class="light-font">'+allProperties[i].message+'</td></tr>';
+    }
+
+    htmlElement.innerHTML = htmlElementVar;
+}
+
+//Show tickets wizard ends (backend)
+
+//Show all agencies wizard starts (backend)
+
+function showAgencies(){
+    const http = new XMLHttpRequest();
+    try{
+        http.open('GET',url+'/v1/agencies');
+        http.setRequestHeader('Content-type', 'application/json');
+        http.send();
+        http.onload = function(){
+            if(http.status != 200 && http.status != 201){
+                console.log('API status: '+http.status);
+            }
+            else{
+                var res = JSON.parse(http.response);
+                allPropertiesCount = res.count;
+                allProperties = res.data;
+                showAllAgencies();
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+function showAllAgencies(){
+    var htmlElement = document.getElementById('htmlElement');
+    var htmlElementVar = '';
+
+    for(i=0; i<allProperties.length; i++){
+        htmlElementVar = htmlElementVar+'<div class="col-xl-4 col-md-6 wow fadeInUp"><div class="property-box"><div class="agent-image"><div><img src="'+allProperties[i].media[0]+
+            '" class="bg-img" alt=""><div class="agent-overlay"></div><div class="overlay-content"><ul><li><a href="tel:'+allProperties[i].phone+'"<i class="fas fa-phone-alt"></i></a></li><li><a href="mailto:'+
+            allProperties[i].email+'"<i class="fas fa-envelope"></i></a></li></ul><span>Connect</span></div></div></div><div class="agent-content"><h3>'+allProperties[i].name+
+            '</h3><ul class="agent-contact"><li><i class="fas fa-phone-alt"></i> <span class="character">'+allProperties[i].phone+'</span></li><li><i class="fas fa-envelope"></i> '+allProperties[i].email+
+            '</li></ul><a href="edit-agent.html?'+allProperties[i]._id+'">Edit Agency <i class="fas fa-arrow-right"></i></a><a href=javascript:deleteAgency("'+allProperties[i]._id+'")>Delete Agency <i class="fas fa-arrow-right"></i></a></div></div></div>';
+    }
+
+    htmlElement.innerHTML = htmlElementVar;
+    loadScript();
+}
+
+//Show all agencies wizard ends (backend)
+
+//Add agency wizard starts (backend)
+
+function addAgency(){
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+
+    var submitText = document.getElementById('submitText');
+    var submitSpinner = document.getElementById('submitSpinner');
+
+    var totalFiles = document.getElementById('multiFileUpload').files.length;
+    var formdata = new FormData();
+
+    submitText.innerText = 'Submitting...';
+    submitSpinner.style.display = 'inline-block';
+
+    formdata.append("name", name);
+    formdata.append("phone", phone);
+    formdata.append("email", email);
+    
+    if(totalFiles > 0){
+        for (i = 0; i < totalFiles; i++) {
+            formdata.append("media", document.getElementById('multiFileUpload').files[i], document.getElementById('multiFileUpload').files[i].name);
+        }
+    }
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    fetch(url+'/v1/agencies', requestOptions)
+    .then(response => {
+        if(response.status != 200 && response.status != 201){
+            console.log('API status: '+response.status);
+            submitText.innerText = 'Submit';
+            submitSpinner.style.display = 'none';
+        }else{
+            submitText.innerText = 'Submit';
+            submitSpinner.style.display = 'none';
+            window.location.href = 'all-agents.html';
+        }
+    })
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error)
+    );
+}
+
+//Add agency wizard ends (backend)
+
+//Edit agency wizard starts (backend)
+
+function editAgency(id){
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+
+    var submitText = document.getElementById('submitText');
+    var submitSpinner = document.getElementById('submitSpinner');
+
+    var totalFiles = document.getElementById('multiFileUpload').files.length;
+    var formdata = new FormData();
+
+    submitText.innerText = 'Submitting...';
+    submitSpinner.style.display = 'inline-block';
+
+    formdata.append("name", name);
+    formdata.append("phone", phone);
+    formdata.append("email", email);
+
+    if(totalFiles > 0){
+        for (i = 0; i < totalFiles; i++) {
+            formdata.append("media", document.getElementById('multiFileUpload').files[i], document.getElementById('multiFileUpload').files[i].name);
+        }
+    }
+
+    var requestOptions = {
+    method: 'PUT',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    fetch(url+'/v1/agencies', requestOptions)
+    .then(response => {
+        if(response.status != 200 && response.status != 201){
+            console.log('API status: '+response.status);
+            submitText.innerText = 'Submit';
+            submitSpinner.style.display = 'none';
+            console.log(response);
+        }else{
+            submitText.innerText = 'Submit';
+            submitSpinner.style.display = 'none';
+            window.location.href = 'all-agents.html';
+        }
+    })
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error)
+    );
+}
+
+//Edit agency wizard ends (backend)
+
+//Delete agency wizard starts (backend)
+
+function deleteAgency(id){
+    const http = new XMLHttpRequest();
+    try{
+        http.open('POST',url+'/v1/agencies/'+id);
+        http.setRequestHeader('Content-type', 'application/json');
+        http.send();
+        http.onload = function(){
+            if(http.status != 200 && http.status != 201){
+                console.log('API status: '+http.status);
+            }
+            else{
+                window.location.href = 'all-agents.html';
+            }
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+//Delete agency wizard ends (backend)
